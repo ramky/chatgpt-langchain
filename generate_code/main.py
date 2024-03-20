2d09067869d139eae36812cde62c9055896efc37
Ram Iyer <ramky.iyer@gmail.com>
2024-03-20 06:02:39 -0400

    using new Pipfile and requirements from author

--- generate_code/main.py
+++ generate_code/main.py
@@ -1,7 +1,15 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
+
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", default="Return a list of numbers")
+parser.add_argument("--language", default="python")
+args = parser.parse_args()
+
 
 llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 
@@ -10,6 +18,26 @@ code_prompt = PromptTemplate(
     input_variables=["language", "task"],
 )
 
-code_chain = LLMChain(llm=llm, prompt=code_prompt)
-result = code_chain({"language": "python", "task": "return a list of 10 numbers"})
-print(result)
+test_prompt = PromptTemplate(
+    template="Write a test for the following {language} code: {code}",
+    input_variables=["language", "code"],
+)
+
+code_chain = LLMChain(llm=llm, prompt=code_prompt, output_key="code")
+
+test_chain = LLMChain(llm=llm, prompt=test_prompt, output_key="test")
+
+# result = code_chain({"language": args.language, "task": args.task})
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["task", "language"],
+    output_variables=["code", "test"],
+)
+
+result = chain({"language": args.language, "task": args.task})
+print(">>>> GENERATED CODE")
+print(result["code"])
+
+print(">>>> GENERATED test")
+print(result["test"])
