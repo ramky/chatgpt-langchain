735fd478599bd0cb2c471f79a479f2cd65042ac9
Ram Iyer <ramky.iyer@gmail.com>
2024-03-25 09:27:46 -0400

    using vector store to make similarity search

--- facts/main.py
+++ facts/main.py
@@ -1,8 +1,9 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings  # 1536 embeds - will change in future
+from langchain_openai import OpenAIEmbeddings  # 1536 embeds - will change in future
+from langchain.vectorstores.chroma import Chroma
 
-DEBUG = True
+DEBUG = False
 
 loader = TextLoader("facts.txt")
 
@@ -19,3 +20,13 @@ if DEBUG:
     for doc in docs:
         print(doc.page_content)
         print("\n")
+
+db = Chroma.from_documents(docs, embedding=embeddings, persist_directory="emb")
+
+prompt = "What is an interesting fact about the English language?"
+results = db.similarity_search_with_score(prompt, k=1)
+
+for result in results:
+    print("\n")
+    # print(result[1])
+    print(result[0].page_content)
