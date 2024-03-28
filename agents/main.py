9f79c838ccbdb9afae31415ace915f9afbdb58d8
Ram Iyer <ramky.iyer@gmail.com>
2024-03-27 20:52:01 -0400

    adding tool to describe table with columns

--- agents/main.py
+++ agents/main.py
@@ -5,20 +5,25 @@ from langchain.prompts import (
     MessagesPlaceholder,
 )
 
+from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
 
 
 chat = ChatOpenAI()
+tables = list_tables()
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(
+            content=f"You are an AI that has access to a SQLite database.\n{tables}"
+        ),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),  # serves as memory
     ]
 )
 
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
 agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
 
--- agents/tools/sql.py
+++ agents/tools/sql.py
@@ -4,6 +4,13 @@ from langchain.tools import Tool
 conn = sqlite3.connect("db.sqlite")
 
 
+def list_tables():
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_master where type='table';")
+    rows = c.fetchall()
+    return "\n".join(row[0] for row in rows if row[0] is not None)
+
+
 def run_sqlite_query(query):
     c = conn.cursor()
     try:
@@ -15,4 +22,20 @@ def run_sqlite_query(query):
 
 run_query_tool = Tool.from_function(
     name="run_sqlite_query", description="Run a sqlite query.", func=run_sqlite_query
+)
+
+
+def describe_tables(table_names):
+    c = conn.cursor()
+    tables = ". ".join("'" + table + "'" for table in table_names)
+    rows = c.execute(
+        f"SELECT sql FROM sqlite_master WHERE type='table' and name IN ({tables});"
+    )
+    return "\n".join(row[0] for row in rows if row[0] is not None)
+
+
+describe_tables_tool = Tool.from_function(
+    name="describe_tables",
+    description="Given a list of table names, returns the schema of those tables.",
+    func=describe_tables,
 )
