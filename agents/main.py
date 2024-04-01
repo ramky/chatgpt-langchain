9896cb0115ced6d7f84dc6309dbae6b6f4248a29
Ram Iyer <ramky.iyer@gmail.com>
2024-04-01 09:57:02 -0400

    using memory with agents

--- agents/main.py
+++ agents/main.py
@@ -7,6 +7,7 @@ from langchain.prompts import (
 
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
 from tools.report import write_report_tool
@@ -22,17 +23,23 @@ prompt = ChatPromptTemplate(
             "Do not make any assumptions about what tables exist "
             "or what columns exist.  Instead, use the 'describe_tables' function"
         ),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),  # serves as memory
     ]
 )
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
-agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
+agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools, memory=memory)
 
 # agent_executor("How many users are in the database?")
 # agent_executor("How many users have an address?")
-agent_executor(
-    "Summarize the top 5 most popular products.  Write the results to a report file."
-)
+# agent_executor(
+#     "Summarize the top 5 most popular products.  Write the results to a report file."
+# )
+agent_executor("How many orders are there? Write the results to a report file.")
+
+agent_executor("Repeat the exact same process for users.")
