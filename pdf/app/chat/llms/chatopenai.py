f74385ab9ce7b6972115b1fb2d797c0773f456d4
Ram Iyer <ramky.iyer@gmail.com>
2024-04-15 11:43:51 -0400

    adding ConversationslRetreivalChain and wiring up chat in app with it

---
+++ pdf/app/chat/llms/chatopenai.py
@@ -0,0 +1,5 @@
+from langchain.chat_models import ChatOpenAI
+
+
+def build_llm(chat_args):
+    return ChatOpenAI()
--- pdf/app/chat/vector_stores/pinecone.py
+++ pdf/app/chat/vector_stores/pinecone.py
@@ -9,3 +9,10 @@ pc = pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 vector_store = Pinecone.from_existing_index(
     os.getenv("PINECONE_INDEX_NAME"), embeddings
 )
+
+
+def build_retriever(chat_args):
+    search_kwargs = {
+        "filter": {"pdf_id": chat_args.pdf_id},
+    }
+    return vector_store.as_retriever(search_kwargs=search_kwargs)
