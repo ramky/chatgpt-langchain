b7a447b5c2c978be3e56c880df39839b458231ca
Ram Iyer <ramky.iyer@gmail.com>
2024-04-23 10:11:43 -0400

    separating streaming and non streaming model and handle on_llm_new_token for that run_id

--- pdf/app/chat/chat.py
+++ pdf/app/chat/chat.py
@@ -1,4 +1,4 @@
-# from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
@@ -20,10 +20,12 @@ def build_chat(chat_args: ChatArgs):
     """
     retriever = build_retriever(chat_args)
     llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)
 
     return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
+        condense_question_llm=condense_question_llm,
         memory=memory,
         retriever=retriever,
     )
