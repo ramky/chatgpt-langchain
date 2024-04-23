0238e01d35165e827abcfae13d855150ae3cee2d
Ram Iyer <ramky.iyer@gmail.com>
2024-04-23 09:30:39 -0400

    adding flask app_context while spinning up thread

--- pdf/app/chat/chains/streamable.py
+++ pdf/app/chat/chains/streamable.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from langchain.chains import LLMChain
 from queue import Queue
 from threading import Thread
@@ -11,10 +12,11 @@ class StreamableChain:
         queue = Queue()
         handler = StreamingHandler(queue)
 
-        def task():
+        def task(app_context):
+            app_context.push()
             self(input, callbacks=[handler])
 
-        Thread(target=task).start()
+        Thread(target=task, args=[current_app.app_context()]).start()
 
         while True:
             token = queue.get()
