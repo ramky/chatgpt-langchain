dc394a6cf05fa32c4336b68b8ad1ff7af6c2586b
Ram Iyer <ramky.iyer@gmail.com>
2024-04-29 09:43:58 -0400

    using weighted random selection for components

--- pdf/app/chat/chat.py
+++ pdf/app/chat/chat.py
@@ -5,6 +5,7 @@ from app.chat.vector_stores import retriever_map
 from app.chat.llms import llm_map
 from app.chat.memories import memory_map
 from app.web.api import set_conversation_components, get_conversation_components
+from app.chat.score import random_component_by_score
 
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 
@@ -17,6 +18,7 @@ def select_component(component_type, component_map, chat_args):
         return previous_component, builder(chat_args)
     else:
         random_name = random.choice(list(component_map.keys()))
+        random_name = random_component_by_score(component_type, component_map)
         builder = component_map[random_name]
         return random_name, builder(chat_args)
 
--- pdf/app/chat/score.py
+++ pdf/app/chat/score.py
@@ -1,4 +1,35 @@
 from app.chat.redis import client
+import random
+
+
+def random_component_by_score(component_type, component_map):
+    # Ensure component type is 'llm', 'retriever', or 'memory'
+    if component_type not in ["llm", "retriever", "memory"]:
+        raise ValueError("Invalid component type")
+    # From redis, get the hash containting the sum total of scores for each component type
+    values = client.hgetall(f"{component_type}_score_values")
+    # From redis, get the hash containing the number of times each component type has been scored
+    counts = client.hgetall(f"{component_type}_score_counts")
+    # Get all the valid component names from the component_map
+    names = component_map.keys()
+    # Loop over those valid names and calculate the average score for each component
+    # Add average scores to a dictionary
+    avg_scores = {}
+    for name in names:
+        print("got name", name)
+        score = int(values.get(name, 1))
+        count = int(counts.get(name, 1))
+        avg = score / count
+        avg_scores[name] = max(avg, 0.1)
+
+    # Do a weighted random selection
+    sum_scores = sum(avg_scores.values())
+    random_val = random.uniform(0, sum_scores)
+    cummulative = 0
+    for name, score in avg_scores.items():
+        cummulative += score
+        if random_val <= cummulative:
+            return name
 
 
 def score_conversation(
