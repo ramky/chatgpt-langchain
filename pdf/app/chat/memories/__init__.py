2c081c9858abd2002918552e777b2148e3f02e1c
Ram Iyer <ramky.iyer@gmail.com>
2024-04-29 21:53:55 -0400

    adding window memory

--- pdf/app/chat/memories/__init__.py
+++ pdf/app/chat/memories/__init__.py
@@ -1,5 +1,7 @@
 from .sql_memory import build_memory
+from .window_memory import window_build_memory_builder
 
 memory_map = {
     "sql_buffer_memory": build_memory,
+    "sql_window_memory": window_build_memory_builder,
 }
